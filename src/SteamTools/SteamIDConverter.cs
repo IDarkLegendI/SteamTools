8ca561b1db1b3d5410953ad8dd183656a5c4bc8b
DarkLegend <42875136+IDarkLegendI@users.noreply.github.com>
2024-10-17 15:01:22 +0300

    -

---
+++ src/SteamTools/SteamIDConverter.cs
@@ -0,0 +1,16 @@
+﻿namespace SteamTools;
+
+public class SteamIdConverter
+{
+    // Базовое значение для всех SteamID64
+    private const ulong SteamId64Base = 76561197960265728;
+
+    public static uint ConvertSteamId64To32(ulong steamId64)
+    {
+        if (steamId64 < SteamId64Base)
+        {
+            throw new ArgumentOutOfRangeException(nameof(steamId64), "steamID64 меньше минимального значения.");
+        }
+        return (uint)(steamId64 - SteamId64Base);
+    }
+}
---
+++ src/SteamTools/SteamIdExtractor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net.Http;
+using HtmlAgilityPack;
+
+namespace SteamTools;
+
+public class SteamIdExtractor
+{
+    public static async Task<ulong> GetSteamId64FromCustomUrl(string customUrl)
+    {
+        using HttpClient client = new HttpClient();
+        string url = customUrl.EndsWith("/") ? customUrl : customUrl + "/";
+        HttpResponseMessage response = await client.GetAsync(url);
+        string pageContent = await response.Content.ReadAsStringAsync();
+
+        // Используем HtmlAgilityPack для парсинга страницы
+        var htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(pageContent);
+
+        // Находим steamID64 через анализ HTML (обычно содержится в теге <meta>)
+        var metaTag = htmlDoc.DocumentNode.SelectSingleNode("//meta[@property='og:url']");
+        if (metaTag != null)
+        {
+            string profileUrl = metaTag.GetAttributeValue("content", "");
+            if (profileUrl.Contains("profiles"))
+            {
+                string steamId64String = profileUrl.Split(new[] { "profiles/" }, StringSplitOptions.None)[1].TrimEnd('/');
+                return ulong.Parse(steamId64String);
+            }
+        }
+
+        throw new Exception("SteamID64 не найден");
+    }
+    
+    
+    public static ulong ExtractSteamId64FromUrl(string url)
+    {
+        // Пример парсинга URL
+        string steamId64String = url.Split(new[] { "profiles/" }, StringSplitOptions.None)[1].TrimEnd('/');
+        return ulong.Parse(steamId64String);
+    }
+    
+    public static async Task<ulong> GetSteamId64FromCustomUrl(string customUrl, HttpClient client)
+    {
+        string url = customUrl.EndsWith("/") ? customUrl : customUrl + "/";
+        HttpResponseMessage response = await client.GetAsync(url);
+        string pageContent = await response.Content.ReadAsStringAsync();
+
+        var htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(pageContent);
+
+        var metaTag = htmlDoc.DocumentNode.SelectSingleNode("//meta[@property='og:url']");
+        if (metaTag != null)
+        {
+            string profileUrl = metaTag.GetAttributeValue("content", "");
+            if (profileUrl.Contains("profiles"))
+            {
+                string steamID64String = profileUrl.Split(new[] { "profiles/" }, StringSplitOptions.None)[1].TrimEnd('/');
+                return ulong.Parse(steamID64String);
+            }
+        }
+
+        throw new Exception("SteamID64 не найден");
+    }
+}
